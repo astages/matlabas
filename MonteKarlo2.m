d18899f9ad13ebbe2d7de66ca97177f2275a91d1
Asta Geseviciute <kramtoske1418@gmail.com>
2013-03-28 13:04:35 +0200

    Pradedas Adaptyvus Monte Carlo. Baigtas grafikas

--- MonteKarlo2.m
+++ MonteKarlo2.m
@@ -5,14 +5,14 @@ a=-10;
 b=10;
 n=2;%dimensija
 k=100;%tasku (vektoriu)skaicius
-x= a + (b-a).* rand(100,n) %perdaryti kad generuotu dvimacius
+x= a + (b-a).*rand(100,n); %perdaryti kad generuotu dvimacius
 for i=1:k
-f(i)=sincos2(x(i,:));
+    f(i)=sincos2(x(i,:));
 end
-[fMin,indMin] = min(f);
+[fMin,indMin] = min(f)
 [fMax,indMax] = max(f);
-xMin = x(indMin,:); %pritaikyti dvimaciui
+xMin = x(indMin,:) %pritaikyti dvimaciui
 xMax = x(indMax,:);%pritaikyti dvimaciui
-fprintf('surastas min=%6.4f, taske x=(%6.4f, %6.4f)\n',fMin,xMin(1),xMin(2));
-fprintf('surastas max=%6.4f, taske x=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
+%fprintf('surastas min=%6.4f, taske x=(%6.4f, %6.4f)\n',fMin,xMin(1),xMin(2));
+%fprintf('surastas max=%6.4f, taske x=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
 grafika2 
--- grafika2.m
+++ grafika2.m
@@ -1,4 +1,3 @@
-x1=-10:0.2:10;
-x2=x1;
-[X1,X2]=meshgrid(x1,x2);%padaryt, kad sugeneruotu dvimaciu tasku tinkleli
-mesh(sincos2(x,:)); %plot netiks: surf, mesh komandospadaryti, kad breztu dvimate funkcija
+[x,y]=meshgrid(-10:.2:10);%padaryt, kad sugeneruotu dvimaciu tasku tinkleli
+f=sin(x)+cos(y);
+surf(x,y,f); %plot netiks: surf, mesh komandospadaryti, kad breztu dvimate funkcija
